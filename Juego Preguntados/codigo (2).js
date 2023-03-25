b996d11058b8bb1c8813fd73dd06725aed9b548f
maximonahuel <il7600724@gmail.com>
2023-03-25 20:44:55 -0300

    Initial commit

---
+++ Juego Preguntados/codigo (2).js
@@ -0,0 +1,185 @@
+var ejercicio = parseInt(prompt("1-elige 1 si crees que argentina gano 1 mundial ,o 2 si crees que gano 2 mundiales "))
+var perdio = 1 ;
+var gano = 2 ;
+if ( ejercicio == 1) {
+ganoo = perdio
+}
+else if (ejercicio == 2){ ganoo = gano
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>1-esta mal,no gano <strong> " + ganoo + " mundial, gano 2.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>1-si, esta bien, gano<strong> " + ganoo + " mundiales. </strong></p>");}
+
+
+
+
+
+var ejercicio = parseInt(prompt("2-elige 1 si crees que argentina es el pais mas poblado de america latina ,o 2 si crees que brazil es el pais mas poblado de america latina "))
+var argentinaa  = 45.38 ;
+var brazill = 212.6 ;
+if ( ejercicio == 1) {
+ganoo = argentinaa
+}
+else if (ejercicio == 2){ ganoo = brazill
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>2-esta mal,argentina tiene de poblacion <strong> " + ganoo + " millones y brazil tiene 212 millones.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>2-si, esta bien,brazil tiene de poblacion <strong> " + ganoo + " millones. </strong></p>");}
+
+
+
+
+
+var ejercicio = parseInt(prompt("3-elige 1 si crees que messi es el jugador con mas balones de oro ,o 2 si crees que fue pele"))
+var masbalones = 7 ;
+var menosbalones = 0 ;
+if ( ejercicio == 1) {
+ganoo = masbalones
+}
+else if (ejercicio == 2){ ganoo = menosbalones
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>3-si, esta bien,messi gano<strong> " + ganoo + " balones de oro.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>3-esta mal,pele gano<strong> " + ganoo + " balones de oro, y messi 7 balones de oro</strong></p>");}
+
+
+
+
+
+var ejercicio = parseInt(prompt("4-elige 1 si crees que playstation fue la plataforma que mas consolas de videojuegos vendio ,o 2 si crees que fue xbox "))
+var play = 585 ;
+var xbox = 177 ;
+if ( ejercicio == 1) {
+ganoo = play
+}
+else if (ejercicio == 2){ ganoo = xbox
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>4-si, esta bien,playstation vendio <strong> " + ganoo + " millones de consolas.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>4-no, esta mal,xbox vendio<strong> " + ganoo + " millones de consolas,y playstation vendio 585 millones. </strong></p>");}
+
+
+
+
+
+var ejercicio = parseInt(prompt("5-elige 1 si crees que Michael Jordan gano 10 campeonatos de la NBA,o 2 si crees que gano 6 campeonatos de la NBA"))
+var perdio = 10 ;
+var gano = 6 ;
+if ( ejercicio == 1) {
+ganoo = perdio
+}
+else if (ejercicio == 2){ ganoo = gano
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>5-esta mal,no gano <strong> " + ganoo + " campeonatos de la NBA, gano 6.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>5-si, esta bien, gano<strong> " + ganoo + " campeonatos de la NBA. </strong></p>");}
+
+
+
+
+
+var ejercicio = parseInt(prompt("6-elige 1 si crees que PewDiePie es el youtuber mas famoso,o 2 si crees que es MrBeast"))
+var gana = 111 ;
+var pierde = 105 ;
+if ( ejercicio == 1) {
+ganoo = gana
+}
+else if (ejercicio == 2){ ganoo = pierde
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>6-si, esta bien,PewDiePie tiene<strong> " + ganoo + " millones de suscriptores.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>6-no, esta mal,MrBeast tiene <strong> " + ganoo + " millones de suscriptores,y PewDiePie tiene 111. </strong></p>");}
+
+
+
+
+
+var ejercicio = parseInt(prompt("7-elige 1 si crees que Marvel Comics numero uno es el comic mas caro de Marvel,o 2 si crees que es Capitan America numero uno"))
+var gano = 859 ;
+var pierdo = 629 ;
+if ( ejercicio == 1) {
+ganoo = gano
+}
+else if (ejercicio == 2){ ganoo = pierdo
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>7-si,esta bien,se vende a<strong> " + ganoo + " mil dolares.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>7-no,esta mal,se vende a<strong> " + ganoo + " mil dolares,mientras Marvel Comics numero uno se vende a 859 mil dolares. </strong></p>");}
+
+
+
+
+
+var ejercicio = prompt("8-elige 1 si crees que china es el pais mas poblado,o 2 si crees que brazil es el pais mas poblado")
+var chinaa  = 1.402 ;
+var brazill = 212.6 ;
+if ( ejercicio == 1) {
+ganoo = chinaa
+}
+else if (ejercicio == 2){ ganoo = brazill
+}
+var peso_final;
+ganoo = (ganoo);
+if (ejercicio == 1){
+document.write("<p>8-si, esta bien,china tiene de poblacion<strong> " + ganoo + " mil millones.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>8-no, esta mal, brazil tiene de poblacion<strong> " + ganoo + " millones y china tiene 1.402 mil millones. </strong></p>");}
+
+
+
+
+
+var ejercicio = parseInt(prompt("9-elige 1 si crees que existen 5 colores primarios,o 2 si crees que existen 3 colores primarios"))
+var perdio = 5 ;
+var gano = 3 ;
+if ( ejercicio == 1) {
+ganoo = perdio
+}
+else if (ejercicio == 2){ ganoo = gano
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>9-no, esta mal,no hay <strong> " + ganoo + " colores primarios, hay 3.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>9-si, esta bien, hay<strong> " + ganoo + " colores primarios. </strong></p>");}
+
+
+
+
+
+var ejercicio = parseInt(prompt("10-elige 1 si crees que la ciudad de mar del plata es mas grande que la plata,o 2 si crees que es la plata mas grande que mar del plata"))
+var marr = 79 ;
+var plataa = 926 ;
+if ( ejercicio == 1) {
+ganoo = marr
+}
+else if (ejercicio == 2){ ganoo = plataa
+}
+var peso_final;
+ganoo = parseInt(ganoo);
+if (ejercicio == 1){
+document.write("<p>10-no, esta mal,mar del plata tiene <strong> " + ganoo + " kilometros, y la plata tiene 926 kilometros.</strong></p>");}
+else if (ejercicio == 2){
+document.write("<p>10-si, esta bien,la plata tiene<strong> " + ganoo + " kilometros. </strong></p>");} ;
